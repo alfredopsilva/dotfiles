0673df6dc3225afa60ae6f15b9aa517183240a98
Alfredo Silva <a.alfredops@gmail.com>
2024-05-05 18:58:35 -0400

    Commiting Changes

--- lua/fractal/plugins/lualine.lua
+++ lua/fractal/plugins/lualine.lua
@@ -1,71 +1,113 @@
+-- return {
+--   "nvim-lualine/lualine.nvim",
+--   dependencies = { "nvim-tree/nvim-web-devicons" },
+--   config = function()
+--     local lualine = require("lualine")
+--     local lazy_status = require("lazy.status") -- to configure lazy pending updates count
+--
+--     local colors = {
+--       blue = "#65D1FF",
+--       green = "#3EFFDC",
+--       violet = "#FF61EF",
+--       yellow = "#FFDA7B",
+--       red = "#FF4A4A",
+--       fg = "#c3ccdc",
+--       bg = "#112638",
+--       inactive_bg = "#2c3043",
+--     }
+--
+--     local my_lualine_theme = {
+--       normal = {
+--         a = { bg = colors.blue, fg = colors.bg, gui = "bold" },
+--         b = { bg = colors.bg, fg = colors.fg },
+--         c = { bg = colors.bg, fg = colors.fg },
+--       },
+--       insert = {
+--         a = { bg = colors.green, fg = colors.bg, gui = "bold" },
+--         b = { bg = colors.bg, fg = colors.fg },
+--         c = { bg = colors.bg, fg = colors.fg },
+--       },
+--       visual = {
+--         a = { bg = colors.violet, fg = colors.bg, gui = "bold" },
+--         b = { bg = colors.bg, fg = colors.fg },
+--         c = { bg = colors.bg, fg = colors.fg },
+--       },
+--       command = {
+--         a = { bg = colors.yellow, fg = colors.bg, gui = "bold" },
+--         b = { bg = colors.bg, fg = colors.fg },
+--         c = { bg = colors.bg, fg = colors.fg },
+--       },
+--       replace = {
+--         a = { bg = colors.red, fg = colors.bg, gui = "bold" },
+--         b = { bg = colors.bg, fg = colors.fg },
+--         c = { bg = colors.bg, fg = colors.fg },
+--       },
+--       inactive = {
+--         a = { bg = colors.inactive_bg, fg = colors.semilightgray, gui = "bold" },
+--         b = { bg = colors.inactive_bg, fg = colors.semilightgray },
+--         c = { bg = colors.inactive_bg, fg = colors.semilightgray },
+--       },
+--     }
+--
+--     -- configure lualine with modified theme
+--     lualine.setup({
+--       options = {
+--         theme = my_lualine_theme,
+--       },
+--       sections = {
+--         lualine_x = {
+--           {
+--             lazy_status.updates,
+--             cond = lazy_status.has_updates,
+--             color = { fg = "#ff9e64" },
+--           },
+--           { "encoding" },
+--           { "fileformat" },
+--           { "filetype" },
+--         },
+--       },
+--     })
+--   end,
+-- }
 return {
   "nvim-lualine/lualine.nvim",
-  dependencies = { "nvim-tree/nvim-web-devicons" },
-  config = function()
-    local lualine = require("lualine")
-    local lazy_status = require("lazy.status") -- to configure lazy pending updates count
-
-    local colors = {
-      blue = "#65D1FF",
-      green = "#3EFFDC",
-      violet = "#FF61EF",
-      yellow = "#FFDA7B",
-      red = "#FF4A4A",
-      fg = "#c3ccdc",
-      bg = "#112638",
-      inactive_bg = "#2c3043",
-    }
-
-    local my_lualine_theme = {
-      normal = {
-        a = { bg = colors.blue, fg = colors.bg, gui = "bold" },
-        b = { bg = colors.bg, fg = colors.fg },
-        c = { bg = colors.bg, fg = colors.fg },
+  dependencies = {
+    "nvim-tree/nvim-web-devicons",
+    "meuter/lualine-so-fancy.nvim",
+  },
+  opts = {
+    options = {
+      theme = "seoul256",
+      component_separators = { left = "│", right = "│" },
+      section_separators = { left = "", right = "" },
+      globalstatus = true,
+      refresh = {
+        statusline = 100,
       },
-      insert = {
-        a = { bg = colors.green, fg = colors.bg, gui = "bold" },
-        b = { bg = colors.bg, fg = colors.fg },
-        c = { bg = colors.bg, fg = colors.fg },
+    },
+    sections = {
+      lualine_a = {
+        { "fancy_mode", width = 3 },
       },
-      visual = {
-        a = { bg = colors.violet, fg = colors.bg, gui = "bold" },
-        b = { bg = colors.bg, fg = colors.fg },
-        c = { bg = colors.bg, fg = colors.fg },
+      lualine_b = {
+        { "fancy_branch" },
+        { "fancy_diff" },
       },
-      command = {
-        a = { bg = colors.yellow, fg = colors.bg, gui = "bold" },
-        b = { bg = colors.bg, fg = colors.fg },
-        c = { bg = colors.bg, fg = colors.fg },
+      lualine_c = {
+        { "fancy_cwd", substitute_home = true },
       },
-      replace = {
-        a = { bg = colors.red, fg = colors.bg, gui = "bold" },
-        b = { bg = colors.bg, fg = colors.fg },
-        c = { bg = colors.bg, fg = colors.fg },
+      lualine_x = {
+        { "fancy_macro" },
+        { "fancy_diagnostics" },
+        { "fancy_searchcount" },
+        { "fancy_location" },
       },
-      inactive = {
-        a = { bg = colors.inactive_bg, fg = colors.semilightgray, gui = "bold" },
-        b = { bg = colors.inactive_bg, fg = colors.semilightgray },
-        c = { bg = colors.inactive_bg, fg = colors.semilightgray },
+      lualine_y = {
+        { "fancy_filetype", ts_icon = "" },
       },
-    }
-
-    -- configure lualine with modified theme
-    lualine.setup({
-      options = {
-        theme = my_lualine_theme,
+      lualine_z = {
+        { "fancy_lsp_servers" },
       },
-      sections = {
-        lualine_x = {
-          {
-            lazy_status.updates,
-            cond = lazy_status.has_updates,
-            color = { fg = "#ff9e64" },
-          },
-          { "encoding" },
-          { "fileformat" },
-          { "filetype" },
-        },
-      },
-    })
-  end,
+    },
+  },
 }
